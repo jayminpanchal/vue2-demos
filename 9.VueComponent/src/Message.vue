18af00538c32dc90ddb952184d814d190b0726a1
Jaymin <jayminpanchal10@gmail.com>
2016-12-31 19:19:18 +0530

    Input component with two way data binding

--- 9.VueComponent/src/Message.vue
+++ 9.VueComponent/src/Message.vue
@@ -1,5 +1,15 @@
 <template>
     <div>
         <h1>This is message from Message.vue</h1>
+        <app-input></app-input>
     </div>
 </template>
+
+<script>
+    import Input from './Input.vue';
+    export default {
+        components: {
+            'app-input': Input
+        }
+    }
+</script>

dfa5a2ff954f7444c439b200e2c8cfb73c894097
Jaymin <jayminpanchal10@gmail.com>
2017-01-01 23:59:00 +0530

    Basic routing setup

---
+++ 11.Routing/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+        <h1>{{ msg }}</h1>
+        <hr/>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        data () {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
---
+++ 11.Routing/src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import Users from './Users.vue';
+import Home from './Home.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+    {path: '/users', component: Users},
+    {path: '/', component: Home}
+];
+const router = new VueRouter({
+    routes
+});
+new Vue({
+    el: '#app',
+    router,
+    render: h => h(App)
+});

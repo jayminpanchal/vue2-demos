ae96d58f09dbf53a68ca10b88854ca4db7a73b31
Jaymin <jayminpanchal10@gmail.com>
2017-01-02 00:07:33 +0530

    Added link for routing

--- 11.Routing/src/App.vue
+++ 11.Routing/src/App.vue
@@ -2,6 +2,9 @@
     <div id="app">
         <h1>{{ msg }}</h1>
         <hr/>
+        <router-link to="/">Home</router-link>
+        <router-link to="/users">Users</router-link>
+        <hr/>
         <router-view></router-view>
     </div>
 </template>

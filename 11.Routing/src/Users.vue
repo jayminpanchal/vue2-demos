ae693605aa4cd89d4b7983b3fad9c25886cc2824
Jaymin <jayminpanchal10@gmail.com>
2017-01-02 00:31:35 +0530

    Manually push the routes

--- 11.Routing/src/Users.vue
+++ 11.Routing/src/Users.vue
@@ -2,5 +2,15 @@
     <div>
         <h1>User Page</h1>
         <p>User Id id:{{$route.params.userId}}</p>
+        <button @click="goHome">Go home</button>
     </div>
 </template>
+<script>
+    export default{
+        methods: {
+            goHome(){
+                this.$router.push("/");
+            }
+        }
+    }
+</script>
